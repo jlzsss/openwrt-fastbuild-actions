60a0ec435dfdf24c18039ad931f933cd07902243
jlzsss <jlz19821031@gmail.com>
2021-09-13 20:23:12 +0800

    Update init_runner.sh

--- scripts/host/init_runner.sh
+++ scripts/host/init_runner.sh
@@ -158,11 +158,6 @@ prepare_dirs() {
   sudo chmod 777 "${HOST_TMP_DIR}"
 }
 
-copy_key() {
-cp -rf /home/builder/key-build /home/builder/openwrt/key-build
-cp -rf /home/builder/key-build.pub /home/builder/openwrt/key-build.pub
-}
-
 main() {
   set -eo pipefail
   if [ "$1" = "build" ]; then
@@ -178,7 +173,6 @@ main() {
   update_builder_info
   check_validity
   prepare_dirs
-  copy_key
 }
 
 if [ "x$1" = "xmain" ]; then
