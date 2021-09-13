554733b66256c30430913003ece7e6bd7dc6b68d
jlzsss <jlz19821031@gmail.com>
2021-09-13 17:26:04 +0800

    Add files via upload

---
+++ user/x86_64/custom.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "=============================================="
+# git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+# git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
+# git clone https://github.com/jlzsss/luci-app-shadowsocksr.git package/luci-app-shadowsocksr
+# git clone https://github.com/jlzsss/openwrt-dnsmasq-extra.git package/openwrt-dnsmasq-extra
+# git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+# git clone https://github.com/jlzsss/project-lede.git package/lede
+rm -rf package/diy/luci-app-dockerman
+rm -rf package/diy/OpenAppFilter
+rm -rf package/lean/luci-theme-argon
+echo "=============================================="
